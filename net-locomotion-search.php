6c34ac74a5e792d2ebf88c8356a9c21ef6630548
Gavin Abson <gavin@netlocomotion.com>
2022-08-20 21:32:47 +0100

    Initial commit

---
+++ net-locomotion-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Search by Net Locomotion
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     net-locomotion-search
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Net_Locomotion_Search
+ */
+
+// Your code starts here.
